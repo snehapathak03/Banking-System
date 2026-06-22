627b3c8a981a9fe1880442c0884e4d5a27bbd1c0
snehapathak03 <snehapathak1303@gmail.com>
2026-06-22 11:42:26 +0530

    initial commit

---
+++ src/BankingManagementSystem/AccountManager.java
@@ -0,0 +1,5 @@
+package BankingManagementSystem;
+
+public class AccountManager {
+
+}
---
+++ src/BankingManagementSystem/BankingApp.java
@@ -0,0 +1,61 @@
+package BankingManagementSystem;
+
+import java.sql.*;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+import static java.lang.Class.forName;
+
+public class BankingApp {
+	private static final String url = "jdbc:mysql://localhost:3306/banking_system";
+	private static final String username = "root";
+	private static final String password = "sona@2003";
+	
+	public static void main(String [] args) throws ClassNotFoundException, SQLException{
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			System.out.println("loaded seccesfully");
+		}catch(ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			Connection conection = DriverManager.getConnection(url, username, password);
+			System.out.println("connected succesfully");
+			Scanner scanner = new Scanner(System.in);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
