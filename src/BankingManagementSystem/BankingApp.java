836baee08ad30d2dc164d21f6f253493288c985c
snehapathak03 <snehapathak1303@gmail.com>
2026-06-24 18:50:26 +0530

    COMPLETED WITH BANKING SYSTEM

--- src/BankingManagementSystem/BankingApp.java
+++ src/BankingManagementSystem/BankingApp.java
@@ -26,40 +26,85 @@ public class BankingApp {
 			Accounts accounts = new Accounts(con, sc);
 			AccountManager accountManager = new AccountManager(con, sc);
 			
+			String email;
+			long account_number;
 			
+			while(true) {
+				System.out.println("---- WELCOME TO BANKING SYSTEM----");
+				System.out.println();
+				System.out.println("1. Register");
+				System.out.println("2. Login");
+				System.out.println("3. Exit");
+				System.out.println("Enter your option : ");
+				int option = sc.nextInt();
+				switch(option) {
+				case 1:
+					user.register();
+					break;
+				case 2:
+					email = user.login();
+					if(email != null) {
+						System.out.println();
+						System.out.println("User Logged In....");
+						if(!accounts.account_exist(email)) {
+							System.out.println();
+							System.out.println("1. Open a new Bank Account");
+							System.out.println("2. Exit");
+							if(sc.nextInt() == 1) {
+								account_number = accounts.open_account(email);
+								System.out.println("Account Created Succesfully");
+								System.out.println("Your Account NUmber is : "+account_number);
+							}else {
+								break;
+							}
+						}
+						account_number = accounts.getAccount_number(email);
+						int option2 =0;
+						while(option2 != 5) {
+							System.out.println();
+							System.out.println("1. Debit Money");
+							System.out.println("2. Credit Money");
+							System.out.println("3. Transfer Money");
+							System.out.println("4. Check Balance");
+							System.out.println("5. Log Out");
+							System.out.println("Enter your option: ");
+							option2 = sc.nextInt();
+							switch(option2) {
+							case 1:
+								accountManager.debit_money(account_number);
+								break;
+							case 2:
+								accountManager.credit_money(account_number);
+								break;
+							case 3:
+								accountManager.transfer_money(account_number);
+								break;
+							case 4:
+								accountManager.getBalance(account_number);
+								break;
+							case 5:
+								break;
+							default :
+								System.out.println("Enter Valid Option!");
+								break;
+							}
+						}
+					}else {
+						System.out.println("Incorrect Email or Password....");
+						break;
+					}
+				case 3:
+					System.out.println("THANK YOU FOR USING BANKING SYSTEM!!!!");
+					System.out.println("Exiting System.....");
+					return;
+				default:
+					System.out.println("Enter Valid Option: ");
+					break;
+				}
+			}
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
